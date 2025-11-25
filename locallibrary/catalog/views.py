78ca907c25e7c1fdd420003f615f7415089bb582
gr434_krovi <gugl6369@gmail.com>
2025-11-25 11:45:55 +0700

    add: create home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+from urllib import request
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Book, Author, BookInstance,Genre
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_djin = Book.objects.filter(title__icontains='djin').count()
+
+    return render(
+        request,
+        'index.html',
+
+        context={'num_books': num_books,'num_instances' : num_instances,
+            'num_instances_available' : num_instances_available, 'num_authors' : num_authors,
+            'num_genres' : num_genres, 'num_djin' : num_djin},
+    )
+
