fee06637517098cc32cad44e9e220f0c2c989a8a
gr434_krovi <gugl6369@gmail.com>
2025-11-26 09:49:24 +0700

    add: sessions

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,14 +12,22 @@ def index(request):
     num_authors = Author.objects.count()
     num_djin = Book.objects.filter(title__icontains='djin').count()
 
-    return render(
-        request,
-        'index.html',
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
 
-        context={'num_books': num_books,'num_instances' : num_instances,
-            'num_instances_available' : num_instances_available, 'num_authors' : num_authors,
-            'num_genres' : num_genres, 'num_djin' : num_djin},
-    )
+    context = {
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+            'num_djin': num_djin,
+            'num_visits': num_visits,
+    }
+
+    return render( request,'index.html', context=context)
+
 
 class BookListView(generic.ListView):
     model = Book
