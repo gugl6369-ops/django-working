5583a80a51439c022343b942efa0f8d708c9b448
gr434_krovi <gugl6369@gmail.com>
2025-12-01 10:18:00 +0700

    create project

---
+++ service/urls.py
@@ -0,0 +1,15 @@
+from django.views.generic import RedirectView
+from xml.etree.ElementInclude import include
+from django.conf.urls.static import static
+from django.conf  import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('enumeration/', include('enumeration.urls')),
+    path('', RedirectView.as_view(url='enumeration/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
