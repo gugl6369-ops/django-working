71285d5bf3957e03419de83e4442a1d326e2fe60
gr434_krovi <gugl6369@gmail.com>
2025-12-01 11:50:39 +0700

    create design and models.py

--- enumeration/models.py
+++ enumeration/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=100, help_text='Название заявки')
+    description = models.TextField(max_length=500, help_text='Описание заявки')
+    category = models.OneToOneField(Category, help_text='Выберете категорию заявки')
+    photo = models.ImageField(help_text='Прикрепите фотографию')
+    status = models.OneToOneField(status)
