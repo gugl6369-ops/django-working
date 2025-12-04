284bc9abac197e6ea7cb1a0f0fc137b1fb6267de
gr434_krovi <gugl6369@gmail.com>
2025-12-04 10:18:46 +0700

    add(form.py):create and delete card

--- enumeration/urls.py
+++ enumeration/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     re_path(r'^$', views.index,  name='index'),
     re_path(r'^application/$', views.ApplicationList.as_view(), name='application_list'),
     re_path(r'^my-applications/$', views.MyApplicationList.as_view(), name='my_applications'),
-    re_path(r'^registration/$', views.consumer_login, name='registration')
+    re_path(r'^applications/(?P<pk>\d+)/delete/$', views.ApplicationDelete.as_view(), name='applications_delete'),
+    re_path(r'^my-applications/create/$', views.ApplicationAdd.as_view(), name='application_create'),
+    re_path(r'^registration/$', views.consumer_login, name='registration'),
 ]
 
