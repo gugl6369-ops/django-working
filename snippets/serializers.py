21b17059248b918c8685b4ab98743e3cf56124b0
elfi <gugl6369@gmail.com>
2025-12-05 22:12:47 +0000

    add(ser, setting, urls): pagination, name for urls for  ser view_name

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+
 from .models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
    owner = serializers.ReadOnlyField(source='owner.username')
 
+   serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
    class Meta:
        model = Snippet
        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
-class UserSerializer(serializers.ModelSerializer):
-   snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+   snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
    class Meta:
        model = User
